0b40cb1a7f374606989596a5aeecc5d952a858f8
Andrew C <lackhand@users.noreply.github.com>
2022-09-12 15:02:41 -0400

    fixbug in action drain
    onenable doesn't work right in editor (so you need to hook into RIOLM and suppress the fake second event). BUT! Can you disable/reenable scriptable objects? I don't really see why not? So!

--- BDUtil.Unity/Packages/org.dawnborn.BDUtil/Runtime/SingletonAsset.cs
+++ BDUtil.Unity/Packages/org.dawnborn.BDUtil/Runtime/SingletonAsset.cs
@@ -1,9 +1,11 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using UnityEngine;
 
 namespace BDUtil
 {
+    // If your singleton needs access to unity ticks, see Ticker.
     [Serializable]
     public class SingletonAsset : ScriptableObject
     {
@@ -20,6 +22,29 @@ namespace BDUtil
         {
             if (!EditorUtils.IsPlayingOrWillChangePlaymode) EditorUtils.RemoveEmptyPreloadedAssets();
         }
+
+        // Workaround for:
+        // https://issuetracker.unity3d.com/issues/application-dot-isplaying-is-false-when-onenable-is-called-from-a-scriptableobject-after-entering-the-play-mode
+        protected virtual void OnRuntimeInitialize(RuntimeInitializeLoadType type) { }
+
+        static void SendOnRuntimeInitialize(RuntimeInitializeLoadType type)
+        { foreach (SingletonAsset asset in singletons.Values) asset.OnRuntimeInitialize(type); }
+
+        [SuppressMessage("IDE", "IDE0051")]
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterAssembliesLoaded)]
+        static void AfterAssembliesLoaded() => SendOnRuntimeInitialize(RuntimeInitializeLoadType.AfterAssembliesLoaded);
+        [SuppressMessage("IDE", "IDE0051")]
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterSceneLoad)]
+        static void AfterSceneLoad() => SendOnRuntimeInitialize(RuntimeInitializeLoadType.AfterSceneLoad);
+        [SuppressMessage("IDE", "IDE0051")]
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+        static void BeforeSceneLoad() => SendOnRuntimeInitialize(RuntimeInitializeLoadType.BeforeSceneLoad);
+        [SuppressMessage("IDE", "IDE0051")]
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSplashScreen)]
+        static void BeforeSplashScreen() => SendOnRuntimeInitialize(RuntimeInitializeLoadType.BeforeSplashScreen);
+        [SuppressMessage("IDE", "IDE0051")]
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.SubsystemRegistration)]
+        static void SubsystemRegistration() => SendOnRuntimeInitialize(RuntimeInitializeLoadType.SubsystemRegistration);
     }
     public class SingletonAsset<T> : SingletonAsset
     where T : SingletonAsset<T>
--- BDUtil.Unity/Packages/org.dawnborn.BDUtil/Samples~/Actions/ActionDrain.cs
+++ BDUtil.Unity/Packages/org.dawnborn.BDUtil/Samples~/Actions/ActionDrain.cs
@@ -1,6 +1,3 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using BDUtil.Pubsub;
 using UnityEngine;
 
@@ -11,30 +8,29 @@ namespace BDUtil
         public Ticker.Event[] TickerEvents;
         public ActionsHead ActionHead;
         readonly Disposes.All unsubscribe = new();
-        bool Begun { get; set; }
-
-        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterSceneLoad)]
-        static void BeginMain() => main.Begin(true);
-        void Begin(bool isBeginMain)
+        bool needsReenable;
+        protected override void OnRuntimeInitialize(RuntimeInitializeLoadType type)
         {
-            Debug.Log($"Starting static drain {this}:{isBeginMain}", this);
-            if (!(Begun |= isBeginMain)) return;
+            if (type != RuntimeInitializeLoadType.AfterSceneLoad) return;
+            needsReenable = false;
+            Begin();
+        }
+        protected override void OnDisable()
+        { unsubscribe.Dispose(); base.OnDisable(); needsReenable = Application.isPlaying; }
+        protected override void OnEnable()
+        { base.OnEnable(); if (needsReenable) Begin(); }
+
+        // I don't think there's any way to suspend/resume a scriptableobject per se BUT:
+        // This is called from OnEnable (which can be called outside of app.isPlaying...)
+        // and from the runtime initialize path.
+        void Begin()
+        {
             unsubscribe.Add(ActionHead.Subscribe(a => a()));
             foreach (Ticker.Event @event in TickerEvents)
             {
                 Debug.Log($"Subscribing {ActionHead}.Pop to {@event}");
                 Ticker.main.Topics[@event].Subscribe(ActionHead.Pop);
             }
-        }
-        protected override void OnEnable()
-        {
-            base.OnEnable();
-            Begin(false);
-        }
-        protected override void OnDisable()
-        {
-            unsubscribe.Dispose();
-            base.OnDisable();
         }
     }
 }
